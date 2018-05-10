a9c0b752b3e3dcd18af0f91c290431bc81da5345
Radek Sztando <radek.sztando@gmail.com>
2018-05-10 20:25:30 +0200

    flyway baseline

--- src/main/java/teachme/Application.java
+++ src/main/java/teachme/Application.java
@@ -2,13 +2,8 @@ package teachme;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import teachme.entities.Concept;
-import teachme.repository.ConceptRepository;
 
 
 @SpringBootApplication
@@ -20,53 +15,4 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Value("${teach-me.numberOfGeneratedRecords}")
-    private int numberOfGeneratedRecords;
-
-
-    @Bean
-    public CommandLineRunner loadData(ConceptRepository repository) {
-        return (args) -> {
-            if (numberOfGeneratedRecords > 0) {
-
-                for (int i = 0; i < numberOfGeneratedRecords; i++) {
-                    repository.save(generateConcept());
-                }
-
-                log.info("Concepts found with findAll():");
-                log.info("-------------------------------");
-                for (Concept concept : repository.findAll()) {
-                    log.info(concept.toString());
-                }
-                log.info("");
-            }
-        };
-    }
-
-    private Concept generateConcept() {
-        int qLength = (int) (Math.random() * 200);
-        int aLength = (int) (Math.random() * 1000);
-        return new Concept(genString(qLength), genString(aLength));
-    }
-
-    private String genString(int length) {
-        StringBuffer buffer = new StringBuffer("");
-
-        while (buffer.length() < length) {
-            int sentenceLength = (int) (Math.random() * 30) + 1;
-            for (int k = 0; k < sentenceLength; k++) {
-                int wordLength = (int) (Math.random() * 12) + 1;
-                for (int i = 0; i < wordLength; i++) {
-                    buffer.append((char) (Math.random() * 26 + 'a'));
-                }
-                if (k != sentenceLength - 1)
-                    buffer.append(' ');
-            }
-            buffer.append('.');
-            buffer.append('\n');
-        }
-        String ret = buffer.toString();
-        ret = ret.substring(0, length);
-        return ret;
-    }
 }
---
+++ src/main/resources/db/migration/V1__Base_version.sql
@@ -0,0 +1,48 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.6.8
+-- Dumped by pg_dump version 9.6.8
+
+
+--
+-- Name: concept; Type: TABLE; Schema: public; Owner: teach-me
+--
+
+CREATE TABLE public.concept (
+    id bigint NOT NULL,
+    definition text,
+    hits bigint NOT NULL,
+    name text
+);
+
+
+ALTER TABLE public.concept OWNER TO "teach-me";
+
+--
+-- Name: hibernate_sequence; Type: SEQUENCE; Schema: public; Owner: teach-me
+--
+
+CREATE SEQUENCE public.hibernate_sequence
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.hibernate_sequence OWNER TO "teach-me";
+
+--
+-- Name: concept concept_pkey; Type: CONSTRAINT; Schema: public; Owner: teach-me
+--
+
+ALTER TABLE ONLY public.concept
+    ADD CONSTRAINT concept_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
