ff5926e3ee0b70ff0f351fe31ad4984f9d09a551
Radek Sztando <radek.sztando@gmail.com>
2018-05-14 14:03:24 +0200

    refactor / cleanup login page

--- src/main/java/teachme/config/MvcConfig.java
+++ src/main/java/teachme/config/MvcConfig.java
@@ -10,7 +10,7 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/learn").setViewName("learn");
-        registry.addViewController("/login").setViewName("login2");
+        registry.addViewController("/login").setViewName("login");
         registry.addViewController("/manage").setViewName("manage");
 
     }
